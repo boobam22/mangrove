599af25f484d43efe6f60db8a093f7b33015f32d
nia <mangrove0720@outlook.com>
2025-10-16 15:53:56 +0800

    rm console and debugger in prod

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   build: {
     rollupOptions: {
       output: {
