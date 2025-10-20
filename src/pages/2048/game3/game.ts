be830bdbc288bd18c890a1ab91aade9938131645
nia <mangrove0720@outlook.com>
2025-10-20 14:42:31 +0800

    GameProvider

---
+++ src/pages/2048/game3/game.ts
@@ -0,0 +1,170 @@
+import { type Direction, default as Board } from './board'
+
+type Action = 'move' | 'swap' | 'remove'
+type History = [Action, string]
+
+export default class Game {
+  private snapshot = ''
+
+  running = false
+  score = 0
+  best = 0
+  moves = 0
+  n_undo = 0
+  n_swap = 0
+  n_remove = 0
+  isWin = false
+  isFailed = false
+  board = new Board()
+  histories = Array<History>()
+
+  private getSnapshot() {
+    const { snapshot, running, histories, ...data } = this // eslint-disable-line
+    return JSON.stringify(data)
+  }
+
+  private loadSnapshot() {
+    if (this.histories.length) {
+      this.snapshot = this.histories.pop()![1]
+      const parsed = JSON.parse(this.snapshot)
+      ;(Object.entries(parsed) as [keyof this, unknown][]).forEach(([k, v]) => {
+        if (k === 'board') {
+          this.board = Board.fromJSON(v as string)
+        } else {
+          this[k] = v as this[typeof k]
+        }
+      })
+    }
+  }
+
+  toJSON() {
+    return JSON.stringify([...this.histories, ['', this.snapshot]])
+  }
+
+  static fromJSON(s: string) {
+    let parsed
+    try {
+      parsed = JSON.parse(s)
+    } catch {}
+
+    const game = new Game()
+    game.newGame()
+
+    if (parsed instanceof Array) {
+      game.snapshot = parsed.pop()
+      game.histories = parsed
+      game.loadSnapshot()
+    }
+    return game
+  }
+
+  newGame() {
+    this.running = true
+    this.score = 0
+    this.moves = 0
+    this.n_undo = 2
+    this.n_swap = 1
+    this.n_remove = 0
+    this.isWin = false
+    this.isFailed = false
+    this.board.data = []
+    this.histories = []
+
+    if (import.meta.env.DEV) {
+      this.n_undo = this.n_swap = this.n_remove = 100
+    }
+
+    this.board.addTile(3)
+    this.snapshot = this.getSnapshot()
+  }
+
+  private record(action: Action) {
+    let len = this.histories.length
+    if (len && action === 'move' && this.histories[len - 1]![0] !== 'move') {
+      while (len) {
+        const type = this.histories.pop()![0]
+        len--
+        if (type !== 'move') {
+          this.n_undo--
+        }
+      }
+    }
+
+    this.histories.push([action, this.snapshot])
+    this.snapshot = this.getSnapshot()
+    if (len > 5) {
+      this.histories.shift()
+    }
+  }
+
+  private clearRemovedTiles() {
+    this.board.data = this.board.data.filter((tile) => !tile.removed)
+  }
+
+  handleMove(direction: Direction) {
+    this.clearRemovedTiles()
+    const { dirty, merges } = this.board.handleMove(direction)
+
+    if (dirty) {
+      this.moves++
+      let score = 0
+      merges.forEach((value) => {
+        score += value
+        if (value === 128) {
+          this.n_undo++
+        } else if (value === 512) {
+          this.n_swap++
+        } else if (value === 1024) {
+          this.n_remove++
+        } else if (!this.isWin && value === 2048) {
+          this.isWin = true
+        }
+      })
+
+      this.score += score
+      this.best = Math.max(this.best, this.score)
+      this.board.addTile()
+
+      if (!this.board.canMove()) {
+        this.isFailed = true
+      }
+
+      this.record('move')
+    }
+
+    return dirty
+  }
+
+  undo() {
+    if (this.n_undo && this.histories.length) {
+      this.n_undo--
+      this.loadSnapshot()
+      return true
+    }
+    return false
+  }
+
+  swapTile(pos1: number, pos2: number) {
+    this.clearRemovedTiles()
+    if (this.n_swap && this.board.swap(pos1, pos2)) {
+      this.moves += 4
+      this.n_swap--
+      this.n_undo++
+      this.record('swap')
+      return true
+    }
+    return false
+  }
+
+  removeTile(pos: number) {
+    this.clearRemovedTiles()
+    if (this.n_remove && this.board.remove(pos)) {
+      this.moves += 8
+      this.n_remove--
+      this.n_undo++
+      this.record('remove')
+      return true
+    }
+    return false
+  }
+}
---
+++ src/pages/2048/game3/index.tsx
@@ -0,0 +1,101 @@
+import { type SetupContext, type Ref, defineComponent, ref, provide, inject } from 'vue'
+
+import { useLocalStorage, LocalStorageProvider } from '@/components/provider/localStorage'
+import Game from './game'
+import { type Direction } from './board'
+import Tile from './tile'
+
+class GameContext {
+  private storage: Ref<string>
+  private game: Game
+
+  running = ref(false)
+  score = ref(0)
+  best = ref(0)
+  moves = ref(0)
+  n_undo = ref(0)
+  n_swap = ref(0)
+  n_remove = ref(0)
+  isWin = ref(false)
+  isFailed = ref(false)
+  board = ref<Tile[]>([])
+
+  constructor(storage: Ref<string>) {
+    this.storage = storage
+    this.game = Game.fromJSON(storage.value)
+    this.markDirty()
+
+    for (const key of Object.getOwnPropertyNames(Object.getPrototypeOf(this))) {
+      const val = (this as any)[key] // eslint-disable-line
+      if (key !== 'constructor' && typeof val === 'function') {
+        ;(this as any)[key] = val.bind(this) // eslint-disable-line
+      }
+    }
+  }
+
+  private markDirty() {
+    this.running.value = this.game.running
+    this.score.value = this.game.score
+    this.best.value = this.game.best
+    this.moves.value = this.game.moves
+    this.n_undo.value = this.game.n_undo
+    this.n_swap.value = this.game.n_swap
+    this.n_remove.value = this.game.n_remove
+    this.isWin.value = this.game.isWin
+    this.isFailed.value = this.game.isFailed
+    this.board.value = this.game.board.data
+
+    this.storage.value = this.game.toJSON()
+  }
+
+  newGame() {
+    this.game.newGame()
+    this.markDirty()
+  }
+
+  handleMove(direction: Direction) {
+    if (this.game.handleMove(direction)) this.markDirty()
+  }
+
+  undo() {
+    if (this.game.undo()) this.markDirty()
+  }
+
+  swapTile(pos1: number, pos2: number) {
+    if (this.game.swapTile(pos1, pos2)) this.markDirty()
+  }
+
+  removeTile(pos: number) {
+    if (this.game.removeTile(pos)) this.markDirty()
+  }
+}
+
+const KEY = '_game'
+
+const _GameProvider = defineComponent({
+  name: 'GameProvider',
+  setup(_, { slots }) {
+    const storage = useLocalStorage()
+    const context = new GameContext(storage)
+
+    provide<GameContext>(KEY, context)
+
+    return () => slots.default?.()
+  },
+})
+
+export function useGame() {
+  const context = inject<GameContext>(KEY)
+  if (context === undefined) {
+    throw new Error('useGame must be used within a GameProvider')
+  }
+  return context
+}
+
+export function GameProvider({ key2 }: { key2: string }, { slots }: SetupContext) {
+  return (
+    <LocalStorageProvider storageKey={key2}>
+      <_GameProvider>{slots.default?.()}</_GameProvider>
+    </LocalStorageProvider>
+  )
+}
